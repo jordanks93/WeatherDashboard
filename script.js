6638e93a22eaa3ac9800ce810b5fbe23b4f51a9a
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2020-11-21 15:27:51 -0500

    added button listeners, capFirstLetter and renderStoredBtns functions

--- script.js
+++ script.js
@@ -1,69 +1,135 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
-
-// api.openweathermap.org/data/2.5/weather?q=Columbus,Ohio&appid=79d6c8a5c213ee81a3e63617ff2d8e9d
-// api.openweathermap.org/data/2.5/weather?q=Columbus,Ohio&units=imperial&appid=79d6c8a5c213ee81a3e63617ff2d8e9d
+// Global Variables
 const t = moment();
 var apiKey = "&appid=79d6c8a5c213ee81a3e63617ff2d8e9d";
+var btnIndex = 0;
+var storageIndex = 0;
+var storageArray = [];
 
-var forecastArr = [];
+// Runs search when button is clicked
+$("#searchButton").click(function (event) {
+   
+    var userCity = $("#searchInput").val().trim();
+    cityWeatherData(userCity);
+    localStorage.setItem(storageIndex, userCity);
+    storageIndex++;
 
-console.log(forecastArr);
+})
 
+// Button listener for previous search inquiries
+$(".searchHistBtnList").click(function (event) {
 
+    var storageSelector = $(event.target).attr("id");
+    console.log(storageSelector);
 
+    var savedSearch = localStorage.getItem(storageSelector);
 
-var cityWeatherData = function (cityState) {
+    cityWeatherData(savedSearch);
+    $("input").val("");
+})
+
+// Capitalizes first letter, used for renderStoredBtns
+function capFirstLetter(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+// Renders stored search inquires as buttons
+function renderStoredBtns() {
+
+    for (i = 0; i < localStorage.length; i++) {
+
+        var storedBtn = localStorage.getItem(i);
+
+        var textArr = storedBtn.split(",");
+
+        var btnText = capFirstLetter(textArr[0]);
+
+        var searchedCityBtn = $('<button type="button" class="btn btn-secondary" id="' + btnIndex + '"></button><br><br>').text(btnText);
+        $(".searchHistBtnList").append(searchedCityBtn);
+
+        btnIndex++;
+
+    }
+}
+
+// Main ajax function, pulls current day weather data, and contains coord data required for nested ajax
+function cityWeatherData(cityState) {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityState + "&units=imperial" + apiKey;
-    console.log(queryURL);
     $.ajax({
         url: queryURL, method: "GET"
     }).then(function (weatherData) {
 
-        $("#cityDateWI").text(weatherData.name + t.format(" MM/DD/YYYY"));
-        $("#temp").append(" " + weatherData.main.temp + "°F");
-        $("#humid").append(" " + weatherData.main.humidity + "%");
-        $("#windSpeed").append(" " + weatherData.wind.speed + " MPH");
+        // create icon to append from api data
+        var icon = weatherData.weather[0].icon + ".png";
+        var iconUrl = "http://openweathermap.org/img/wn/" + icon;
+        var iconEl = $('<img src="' + iconUrl + '" alt="Weather status icon"></img>');
+
+
+        $("#cityDateWI").text(weatherData.name + t.format(" (MM/DD/YYYY)")).append(iconEl);
+        $("#temp").text("Temperature: " + weatherData.main.temp + "°F");
+        $("#humid").text("Humidity: " + weatherData.main.humidity + "%");
+        $("#windSpeed").text("Wind Speed: " + weatherData.wind.speed + " MPH");
+
+        if ($("#searchInput").val() != "") {
+
+            console.log(btnIndex);
+            var searchedCityBtn = $('<button type="button" class="btn btn-secondary" id=' + btnIndex + '></button><br><br>').text(weatherData.name);
+            $(".searchHistBtnList").append(searchedCityBtn);
+            btnIndex++;
+
+        }
 
         // 5 day forecast
         var lat = weatherData.coord.lat;
         var lon = weatherData.coord.lon;
         var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial" + apiKey;
-        console.log(queryURL);
 
         $.ajax({
             url: queryURL, method: "GET"
         }).then(function (forecast) {
 
             //add UV data to current weather, not included in first api call
-            $("#uv").append(" " + forecast.current.uvi);
+            var uvIndex = forecast.current.uvi;
+            var uvEl = $("<span>").text(uvIndex);
+            $("#uv").text("UV Index: ");
+            $("#uv").append(uvEl);
 
-            var count5Day = 5;
-            console.log(forecast.daily[0].temp.day);
-            for (i = 0; i <= count5Day; i++) {
-
-                $("#card-date" + i).append(moment.unix(forecast.daily[i].dt).format("MM/DD/YYYY"));
-                $("#card-temp" + i).append(" " + forecast.daily[i].temp.day + " °F");
-                $("#card-humid" + i).append(" " + forecast.daily[i].humidity + "%");
+            if (uvIndex < 3) {
+                uvEl.addClass("bg-green");
+            }
+            else if (uvIndex >= 3 && uvIndex < 6) {
+                uvEl.addClass("bg-yellow");
+            }
+            else if (uvIndex >= 6 && uvIndex < 8) {
+                uvEl.addClass("bg-orange");
+            }
+            else if (uvIndex >= 8 && uvIndex <= 11) {
+                uvEl.addClass("bg-red");
+            }
+            else if (uvIndex > 11) {
+                uvEl.addClass("bg-violet");
             }
 
-        });
+            var count5Day = 5;
 
-    }); //first ajax end tag
+            for (i = 0; i <= count5Day; i++) {
+
+                // create icon to append from api data
+                var icon = forecast.daily[i].weather[0].icon + ".png";
+                var iconUrl = "http://openweathermap.org/img/wn/" + icon;
+                var iconEl = $('<img src="' + iconUrl + '" alt="Weather status icon"></img>');
+
+                $("#card-date" + i).text(moment.unix(forecast.daily[i].dt).format("MM/DD/YYYY"));
+                $("#iconImg" + i).text("").append(iconEl);
+                $("#card-temp" + i).text("Temp: " + forecast.daily[i].temp.day + " °F");
+                $("#card-humid" + i).text("Humidity: " + forecast.daily[i].humidity + "%");
+            }
+        });
+    }); 
 }
 
+// Runs Columbus, Ohio search by default so page is not empty
 cityWeatherData("Columbus,Ohio");
-
+// Render stored buttons on page load
+renderStoredBtns();
 
 
